df75f81e287357a366d365af43c5d7ec2d2931c1
Sabrina Bosco <sabrina.bosco@citocode.com>
2017-02-02 04:44:23 +0100

    process coffee files to js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
 gulp.task('log', function() {
   gutil.log('workflow works');
+});
+
+gulp.task('coffee', function() {
+
+  gutil.log('coffee task');
+  gulp.src('components/coffee/tagline.coffee')
+    .pipe(coffee({bare: true})
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'));
+
 });
